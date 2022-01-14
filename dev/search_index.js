bfcc70321c7c1275cd07a045f6bd3ee2c7c03c6d
Documenter.jl <documenter@juliadocs.github.io>
2022-01-14 14:43:01 +0000

    build based on 637ab22

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LandRegistryPricePaidDB.jl","page":"LandRegistryPricePaidDB.jl","title":"LandRegistryPricePaidDB.jl","text":"","category":"section"},{"location":"","page":"LandRegistryPricePaidDB.jl","title":"LandRegistryPricePaidDB.jl","text":"Documentation for LandRegistryPricePaidDB.jl","category":"page"},{"location":"","page":"LandRegistryPricePaidDB.jl","title":"LandRegistryPricePaidDB.jl","text":"","category":"page"}]
+}
